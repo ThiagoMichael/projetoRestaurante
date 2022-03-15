bc52a45fc42fb8a578400c389dcabbe33286e671
Thiago Michael <78152544+ThiagoMichael@users.noreply.github.com>
2022-03-15 16:26:50 -0300

    Criação da tela de adicionar e ajustes no código

--- Restaurant/src/app/app.module.ts
+++ Restaurant/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { EstoqueComponent } from './estoque/estoque.component';
 import { NavComponent } from './nav/nav.component';
+import { AdicionarComponent } from './adicionar/adicionar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     EstoqueComponent,
-    NavComponent
+    NavComponent,
+    AdicionarComponent
   ],
   imports: [
     BrowserModule
