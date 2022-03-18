ccdb9496558c5b7826da89d4a036460a476d32cb
Ney Rubson de Sousa e Silva <93494773+NeyRubson@users.noreply.github.com>
2022-03-18 18:11:06 -0300

    Sending-files-static-to-the-server

--- Restaurant/server.js
+++ Restaurant/server.js
@@ -1,21 +1,7 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const http = require('http')
+const PORT = 3000
+const app = require('./config/express')
 
-// Pegando as API routes
-const api = require('./server/routes/api')
-
-// Usando middleware
-app.use(express.static(path.join(__dirname + 'dist')))
-app.use('/api', api)
-
-// Pegando todas os routes requests e retornando eles para o index.html
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'dist/restaurant/index.html'))
-})
-
-const port = process.env.PORT || 8080;
-
-app.listen(8080, (req,res)=>{
-    console.log('+ Servidor rodando na porta: ' + port)
+http.createServer(app).listen(PORT, (req,res)=>{
+    console.log('Servidor iniciado')
 })
